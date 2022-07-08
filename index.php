0c72568c886c8f9f2795bfcd79d7206a33b6cbdf
ali-chavoshi <a.chavoshi68@yahoo.com>
2022-07-08 18:35:33 +0430

    fancybox

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>image store project</title>
+    <script>var baseUrl = "http://localhost/photo-store/"; </script>
+</head>
+<body>
+
+<div class="container">
+    <div class="header">Welcome To Our Image Store</div>
+    <div class="body">
+        <div class="col-md-4 col-sm12 sidebar"></div>
+        <div class="col-md-8 col-sm12 images"></div>
+    </div>
+</div>
+
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
